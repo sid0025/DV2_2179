c51b5921b3dd6b0806b7decdfcd816320871d82a
sid0025 <smad0025@student.monash.edu>
2026-05-20 09:00:26 +1000

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,8 +1,8 @@
-vegaEmbed('#chart1', 'json/Chart 1 - Top 10 Birds by sightings .json').then(function(result) {
+vegaEmbed('#chart1', 'json/chart1_top_species.json').then(function(result) {
 }).catch(console.error);
 
-vegaEmbed('#chart2', 'json/Chart 2 - Birds by peak counts .json').then(function(result) {
+vegaEmbed('#chart2', 'json/chart2_peak_counts.json').then(function(result) {
 }).catch(console.error);
 
-vegaEmbed('#chart3', 'json/Chart 3 - Peak count of Birds in Summer vs Winter .json').then(function(result) {
+vegaEmbed('#chart3', 'json/chart3_seasonal.json').then(function(result) {
 }).catch(console.error);
