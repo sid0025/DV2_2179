3c179b7551a9ba281d28fcd37ff34692258a0ffb
sid0025 <smad0025@student.monash.edu>
2026-05-20 08:52:24 +1000

    Create vega_lite_vis.js

---
+++ vega_lite_vis.js
@@ -0,0 +1,8 @@
+vegaEmbed('#chart1', 'json/Chart 1 - Top 10 Birds by sightings .json').then(function(result) {
+}).catch(console.error);
+
+vegaEmbed('#chart2', 'json/Chart 2 - Birds by peak counts .json').then(function(result) {
+}).catch(console.error);
+
+vegaEmbed('#chart3', 'json/Chart 3 - Peak count of Birds in Summer vs Winter .json').then(function(result) {
+}).catch(console.error);
